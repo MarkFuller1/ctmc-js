aa95bf9157965df02c0721e443be5495d38ede9e
Mark Fuller <Mark_Fuller1@baylor.edu>
2020-03-07 19:52:06 -0600

    fix main

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import * as Requests from '../API/Requests';
+// import { PRIMARY } from "../index";
+
+export default function Main(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
