c2795ca1a82f83ad5bb6e37670f5302e96684030
MarkFuller1 <mark_fuller1@baylor.edu>
2020-03-07 19:51:27 -0600

    drawer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { observer } from '../../node_modules/mobx-react/dist';
-import Main from '../components/Main';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { observer } from "../../node_modules/mobx-react/dist";
+import Main from "../components/main";
+import MiniDrawer from "./../components/Drawer"
 
-const Homepage = observer(function Homepage(props){
-    return(
-        <div>
-            <Main />
-        </div>
-    )
-})
+const Homepage = observer(function Homepage(props) {
+  return (
+    <div>
+        <MiniDrawer/>
+    </div>
+  );
+});
 export default withRouter(Homepage);
