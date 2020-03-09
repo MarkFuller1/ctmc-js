690c48c847045722345c588202a5a402a43a450d
Mark Fuller <Mark_Fuller1@baylor.edu>
2020-03-09 18:29:43 -0500

    asdf

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+export default function Main(props) {
+  return (
+    <div>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+      <h1>asdf</h1>
+    </div>
+  );
+}
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { observer } from "../../node_modules/mobx-react/dist";
-import Main from "../components/main";
+import Main from "../components/Main";
 import MiniDrawer from "./../components/Drawer"
 
 const Homepage = observer(function Homepage(props) {
   return (
     <div>
         <MiniDrawer/>
+        <Main/>
+
     </div>
   );
 });
