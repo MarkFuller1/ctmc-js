fecdb51956aaa2a2317ff8697450328215e1a6f5
david-b3ggs <david_beggs@baylor.edu>
2020-03-10 12:43:40 -0500

    Cleaning up Marks Hacky code

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { observer } from "../../node_modules/mobx-react/dist";
 import MiniDrawer from "./../components/Drawer"
+
+
 const Homepage = observer(function Homepage(props) {
+
   return (
     <div>
         <MiniDrawer/>
